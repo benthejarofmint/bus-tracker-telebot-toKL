0d3155df702b6f3b327d6aaf5ded9c2e1eb3c66f
ben <jwben03@gmail.com>
2026-05-17 16:50:35 +0800

    Update the reminder messages and add google maps link to the 30mins toll checkpoint

--- busToKL.py
+++ busToKL.py
@@ -92,7 +92,7 @@ prompts = {
     "left_my_custom": "Have you left MY Customs?",
     "reached_rest_stop": "Have you reached the rest stop?",
     "left_rest_stop": "Have you left the rest stop?",
-    "at_30_min_mark": "Are you at the toll with many tall yellow building 30mins away from Sunway?",
+    "at_30_min_mark": "Are you at the toll with many tall yellow building 30mins away from Sunway? \n <a href='https://maps.app.goo.gl/1iLLb1uy5Tp2W9uZ9'>📍 View your location from the checkpoint here </a>",
     "reached_sunway": "Have you reached Sunway? 🎉🚌"
 }
 
@@ -477,7 +477,7 @@ def handle_step_callback(call):
             session['awaiting_passenger_count_step'] = step_key
 
             # 🎯 Custom reminder after MY Customs
-            if step_key == "left_sg_custom":
+            if step_key == "left_my_custom":
                 bot.send_message(
                     chat_id,
                     "🔔 *Reminder for Bus IC:*\nPlease put back the event signages at the:\n"
@@ -489,23 +489,23 @@ def handle_step_callback(call):
 
                 bot.send_message(
                     chat_id,
-                    "🔔 *Reminder for Bus IC:*\nPlease remember to do a passport check with everyone in the bus too!\n",
+                    "🔔 *Reminder for Bus IC:*\nPlease remember to do a passport check with everyone in the bus before leaving customs!\n",
                     parse_mode="Markdown"
                 )
 
-            if step_key == "left_my_custom":
+            if step_key == "left_sg_custom":
                 bot.send_message(
                     chat_id,
-                    "🔔 *Reminder for Bus IC:*\nPlease remember to do a passport check with everyone in the bus!\n",
-                    parse_mode="Markdown"
-                )
-            if step_key == "left_my_custom":
-                bot.send_message(
-                    chat_id,
-                    "🔔 *Reminder for Bus IC:*\nPlease bring down at the SG customs:\n"
+                    "🔔 *Reminder for Bus IC:*\nPlease bring down at the MY customs:\n"
                     "- 🪧 *3 Bus Signages (Front, Left, Rear)*\n"
                     "- 😷 *Surgical masks*\n"
                     "- 🎒 *ALL BELONGINGS*",
+                    parse_mode="Markdown"
+                )
+
+                bot.send_message(
+                    chat_id,
+                    "🔔 *Reminder for Bus IC:*\nPlease remember to do a passport check with everyone in the bus before leaving customs!\n",
                     parse_mode="Markdown"
                 )
 
